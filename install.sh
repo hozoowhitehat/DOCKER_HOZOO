74b29c28c8f7f0236bc9b620e0945e6c586465dc
HOZOO WHITE HAT <hozooimut@gmail.com>
2025-03-11 18:13:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 sudo apt update
-sudo apt install xfce4 xfce4-goodies novnc python3-websockify python3-numpy tightvncserver htop nano neofetch -y
+sudo apt install xfce4 xfce4-goodies novnc python3-websockify python3-numpy tightvncserver htop nano neofetch -y novnc.pem -out novnc.pem -days 3650
+USER=root vncserver
+vncserver -kill :1
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
